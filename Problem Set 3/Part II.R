7e3d8de3f0068a95249de9d4b505cc3a84e10f03
TDOlsen19 <td49erz@hotmail.com>
2014-04-20 16:18:32 -0400

    Problem Set 3

---
+++ Problem Set 3/Part II.R
@@ -0,0 +1,347 @@
+# Import Data
+> gdURL <- "http://www.stat.ubc.ca/~jenny/notOcto/STAT545A/examples/gapminder/data/gapminderDataFiveYear.txt"
+> gDat <- read.delim(file = gdURL)
+> str(gDat)
+'data.frame':  1704 obs. of  6 variables:
+  $ country  : Factor w/ 142 levels "Afghanistan",..: 1 1 1 1 1 1 1 1 1 1 ...
+$ year     : int  1952 1957 1962 1967 1972 1977 1982 1987 1992 1997 ...
+$ pop      : num  8425333 9240934 10267083 11537966 13079460 ...
+$ continent: Factor w/ 5 levels "Africa","Americas",..: 3 3 3 3 3 3 3 3 3 3 ...
+$ lifeExp  : num  28.8 30.3 32 34 36.1 ...
+$ gdpPercap: num  779 821 853 836 740 ...
+> 
+  > # Data Aggregation
+  > (snippet <- subset(gDat, country == "Canada"))
+country year      pop continent lifeExp gdpPercap
+241  Canada 1952 14785584  Americas  68.750  11367.16
+242  Canada 1957 17010154  Americas  69.960  12489.95
+243  Canada 1962 18985849  Americas  71.300  13462.49
+244  Canada 1967 20819767  Americas  72.130  16076.59
+245  Canada 1972 22284500  Americas  72.880  18970.57
+246  Canada 1977 23796400  Americas  74.210  22090.88
+247  Canada 1982 25201900  Americas  75.760  22898.79
+248  Canada 1987 26549700  Americas  76.860  26626.52
+249  Canada 1992 28523502  Americas  77.950  26342.88
+250  Canada 1997 30305843  Americas  78.610  28954.93
+251  Canada 2002 31902268  Americas  79.770  33328.97
+252  Canada 2007 33390141  Americas  80.653  36319.24
+> 
+  > library(plyr)
+> library(lattice)
+Warning message:
+  package 'lattice' was built under R version 3.0.3 
+> 
+  > (maxLeByCont <- ddply(gDat, ~continent, summarize, maxlifeExp = max(lifeExp)))
+continent maxlifeExp
+1    Africa     76.442
+2  Americas     80.653
+3      Asia     82.603
+4    Europe     81.757
+5   Oceania     81.235
+> 
+  > str(maxLeByCont)
+'data.frame':	5 obs. of  2 variables:
+  $ continent : Factor w/ 5 levels "Africa","Americas",..: 1 2 3 4 5
+$ maxlifeExp: num  76.4 80.7 82.6 81.8 81.2
+> levels(maxLeByCont$continent)
+[1] "Africa"   "Americas" "Asia"     "Europe"   "Oceania" 
+> 
+  > (minLeBygdpPercap <- ddply(gDat, ~continent, summarize, mingdpPercap = min(gdpPercap)))
+continent mingdpPercap
+1    Africa     241.1659
+2  Americas    1201.6372
+3      Asia     331.0000
+4    Europe     973.5332
+5   Oceania   10039.5956
+> 
+  > ddply(gDat, ~continent, summarize, nUniqCountries = Length(unique(country)))
+Error in eval(expr, envir, enclos) : could not find function "Length"
+> ddply(gDat, ~continent, summarize, nUniqCountries = length(unique(country)))
+continent nUniqCountries
+1    Africa             52
+2  Americas             25
+3      Asia             33
+4    Europe             30
+5   Oceania              2
+> 
+  > ddply(gDat, ~continent, function(x) return(c(nUniqCountries = length(unique(x$country)))))
+continent nUniqCountries
+1    Africa             52
+2  Americas             25
+3      Asia             33
+4    Europe             30
+5   Oceania              2
+> ddply(gDat, ~continent, summarize, minLifeExp = min(lifeExp), maxlifeExp = max(lifeExp), medGdpPercap = median(gdpPercap))
+continent minLifeExp maxlifeExp medGdpPercap
+1    Africa     23.599     76.442     1192.138
+2  Americas     37.579     80.653     5465.510
+3      Asia     28.801     82.603     2646.787
+4    Europe     43.585     81.757    12081.749
+5   Oceania     69.120     81.235    17983.304
+> 
+  > #Putting it all together
+  > jCountry <- "France"
+> (jDat <- subset(gDat, country == jCountry))
+country year      pop continent lifeExp gdpPercap
+529  France 1952 42459667    Europe  67.410  7029.809
+530  France 1957 44310863    Europe  68.930  8662.835
+531  France 1962 47124000    Europe  70.510 10560.486
+532  France 1967 49569000    Europe  71.550 12999.918
+533  France 1972 51732000    Europe  72.380 16107.192
+534  France 1977 53165019    Europe  73.830 18292.635
+535  France 1982 54433565    Europe  74.890 20293.897
+536  France 1987 55630100    Europe  76.340 22066.442
+537  France 1992 57374179    Europe  77.460 24703.796
+538  France 1997 58623428    Europe  78.640 25889.785
+539  France 2002 59925035    Europe  79.590 28926.032
+540  France 2007 61083916    Europe  80.657 30470.017
+> xyplot(lifeExp ~ year, jDat, type = c("p", "r"))
+> jFit <- lm(lifeExp ~ year, jDat)
+> summary(jFit)
+
+Call:
+  lm(formula = lifeExp ~ year, data = jDat)
+
+Residuals:
+  Min       1Q   Median       3Q      Max 
+-0.38013 -0.13894  0.01235  0.14295  0.33486 
+
+Coefficients:
+  Estimate Std. Error t value Pr(>|t|)    
+(Intercept) -397.76460    7.28533  -54.60 1.03e-13 ***
+  year           0.23850    0.00368   64.81 1.86e-14 ***
+  ---
+  Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+
+Residual standard error: 0.22 on 10 degrees of freedom
+Multiple R-squared:  0.9976,	Adjusted R-squared:  0.9974 
+F-statistic:  4200 on 1 and 10 DF,  p-value: 1.863e-14
+
+> (yearMin <- min(gDat$year))
+[1] 1952
+> jFit <- lm(lifeExp ~ I(year - yearMin), jDat)
+> summary(jFit)
+
+Call:
+  lm(formula = lifeExp ~ I(year - yearMin), data = jDat)
+
+Residuals:
+  Min       1Q   Median       3Q      Max 
+-0.38013 -0.13894  0.01235  0.14295  0.33486 
+
+Coefficients:
+  Estimate Std. Error t value Pr(>|t|)    
+(Intercept)       67.79013    0.11949  567.33  < 2e-16 ***
+  I(year - yearMin)  0.23850    0.00368   64.81 1.86e-14 ***
+  ---
+  Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+
+Residual standard error: 0.22 on 10 degrees of freedom
+Multiple R-squared:  0.9976,	Adjusted R-squared:  0.9974 
+F-statistic:  4200 on 1 and 10 DF,  p-value: 1.863e-14
+
+> class(jFit)
+[1] "lm"
+> mode(jFit)
+[1] "list"
+> names(jFit)
+[1] "coefficients"  "residuals"     "effects"       "rank"          "fitted.values" "assign"        "qr"           
+[8] "df.residual"   "xlevels"       "call"          "terms"         "model"        
+> jFit$coefficients
+(Intercept) I(year - yearMin) 
+67.7901282         0.2385014 
+> coef(jFit)
+(Intercept) I(year - yearMin) 
+67.7901282         0.2385014 
+> 
+  > jFun <- function(x) coef(lm(lifeExp ~ I(year - yearMin), x))
+> jFun(jDat)
+(Intercept) I(year - yearMin) 
+67.7901282         0.2385014 
+> jFun <- function(x) {
+  +     estCoefs <- coef(lm(lifeExp ~ I(year - yearMin), x))
+  +     names(estCoefs) <- c("intercept", "slope")
+  +     return(estCoefs)
+  + }
+> jFun(jDat)
+intercept      slope 
+67.7901282  0.2385014 
+> 
+  > jFun(subset(gDat, country == "Canada"))
+intercept      slope 
+68.8838462  0.2188692 
+> jFun(subset(gDat, country == "Uruguay"))
+intercept     slope 
+65.741603  0.183272 
+> jFun(subset(gDat, country == "India"))
+intercept     slope 
+39.269756  0.505321 
+> 
+  > jCoefs <- ddply(gDat, ~country, jFun)
+> str(jCoefs)
+'data.frame':	142 obs. of  3 variables:
+  $ country  : Factor w/ 142 levels "Afghanistan",..: 1 2 3 4 5 6 7 8 9 10 ...
+$ intercept: num  29.9 59.2 43.4 32.1 62.7 ...
+$ slope    : num  0.275 0.335 0.569 0.209 0.232 ...
+> 
+  > tail(jCoefs)
+country intercept       slope
+137          Venezuela  57.51332  0.32972168
+138            Vietnam  39.01008  0.67161538
+139 West Bank and Gaza  43.79840  0.60110070
+140        Yemen, Rep.  30.13028  0.60545944
+141             Zambia  47.65803 -0.06042517
+142           Zimbabwe  55.22124 -0.09302098
+> 
+  > ## realistically, you would read the data from a local file
+  > gdURL <- "http://www.stat.ubc.ca/~jenny/notOcto/STAT545A/examples/gapminder/data/gapminderDataFiveYear.txt"
+> gDat <- read.delim(file = gdURL)
+> ##str(gDat) here when working interactively
+  > yearMin <- min(gDat$year)
+> jFun <- function(x) {
+  +     estCoefs <- coef(lm(lifeExp ~ I(year - yearMin), x))
+  +     names(estCoefs) <- c("intecept", "slope")
+  +     return(estCoefs)
+  + }
+> ## jFun(subset(gDat, country == "India')) to see what it does
+  > jCoefs <- ddply(gDat, ~country, jFun)
+> 
+  > install.packages("xtable", dependencies = TRUE)
+Installing package into 'C:/Users/Trevor/Documents/R/win-library/3.0'
+(as 'lib' is unspecified)
+trying URL 'http://cran.rstudio.com/bin/windows/contrib/3.0/xtable_1.7-3.zip'
+Content type 'application/zip' length 388491 bytes (379 Kb)
+opened URL
+downloaded 379 Kb
+
+package 'xtable' successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+C:\Users\Trevor\AppData\Local\Temp\Rtmp8cl2b6\downloaded_packages
+> library(xtable)
+> 
+  > set.seed(916)
+> foo <- jCoefs[sample(nrow(jCoefs), size = 15), ]
+> foo <- xtable(foo)
+> print(foo, type = "html", include.rownames = FALSE)
+<!-- html table generated in R 3.0.2 by xtable 1.7-3 package -->
+  <!-- Sun Apr 20 14:46:31 2014 -->
+  <TABLE border=1>
+  <TR> <TH> country </TH> <TH> intecept </TH> <TH> slope </TH>  </TR>
+  <TR> <TD> Lebanon </TD> <TD align="right"> 58.69 </TD> <TD align="right"> 0.26 </TD> </TR>
+  <TR> <TD> Senegal </TD> <TD align="right"> 36.75 </TD> <TD align="right"> 0.50 </TD> </TR>
+  <TR> <TD> Dominican Republic </TD> <TD align="right"> 48.60 </TD> <TD align="right"> 0.47 </TD> </TR>
+  <TR> <TD> Oman </TD> <TD align="right"> 37.21 </TD> <TD align="right"> 0.77 </TD> </TR>
+  <TR> <TD> Germany </TD> <TD align="right"> 67.57 </TD> <TD align="right"> 0.21 </TD> </TR>
+  <TR> <TD> Korea, Dem. Rep. </TD> <TD align="right"> 54.91 </TD> <TD align="right"> 0.32 </TD> </TR>
+  <TR> <TD> Mauritius </TD> <TD align="right"> 55.37 </TD> <TD align="right"> 0.35 </TD> </TR>
+  <TR> <TD> Slovak Republic </TD> <TD align="right"> 67.01 </TD> <TD align="right"> 0.13 </TD> </TR>
+  <TR> <TD> Comoros </TD> <TD align="right"> 40.00 </TD> <TD align="right"> 0.45 </TD> </TR>
+  <TR> <TD> Argentina </TD> <TD align="right"> 62.69 </TD> <TD align="right"> 0.23 </TD> </TR>
+  <TR> <TD> Central African Republic </TD> <TD align="right"> 38.81 </TD> <TD align="right"> 0.18 </TD> </TR>
+  <TR> <TD> Ecuador </TD> <TD align="right"> 49.07 </TD> <TD align="right"> 0.50 </TD> </TR>
+  <TR> <TD> West Bank and Gaza </TD> <TD align="right"> 43.80 </TD> <TD align="right"> 0.60 </TD> </TR>
+  <TR> <TD> Egypt </TD> <TD align="right"> 40.97 </TD> <TD align="right"> 0.56 </TD> </TR>
+  <TR> <TD> Myanmar </TD> <TD align="right"> 41.41 </TD> <TD align="right"> 0.43 </TD> </TR>
+  </TABLE>
+  > 
+  > jCoefs <- ddply(gDat, ~country + continent, jFun)
+> str(jCoefs)
+'data.frame':	142 obs. of  4 variables:
+  $ country  : Factor w/ 142 levels "Afghanistan",..: 1 2 3 4 5 6 7 8 9 10 ...
+$ continent: Factor w/ 5 levels "Africa","Americas",..: 3 4 1 1 2 5 4 3 3 4 ...
+$ intecept : num  29.9 59.2 43.4 32.1 62.7 ...
+$ slope    : num  0.275 0.335 0.569 0.209 0.232 ...
+> tail(jCoefs)
+country continent intecept       slope
+137          Venezuela  Americas 57.51332  0.32972168
+138            Vietnam      Asia 39.01008  0.67161538
+139 West Bank and Gaza      Asia 43.79840  0.60110070
+140        Yemen, Rep.      Asia 30.13028  0.60545944
+141             Zambia    Africa 47.65803 -0.06042517
+142           Zimbabwe    Africa 55.22124 -0.09302098
+> 
+  > set.seed(916)
+> foo <- jCoefs[sample(nrow(jCoefs), size = 15), ]
+> foo <- arrange(foo, intercept)
+Error in order(intercept) : object 'intercept' not found
+> foo <- arrange(foo, intercept)
+Error in order(intercept) : object 'intercept' not found
+> foo <- arrange(foo, intecept)
+> ##foo <- foo[order(foo$intercept), ] # an uglier non-plyr way
+  > foo <- xtable(foo)
+> print(foo, type = "html", include.rownames = FALSE)
+<!-- html table generated in R 3.0.2 by xtable 1.7-3 package -->
+  <!-- Sun Apr 20 14:57:34 2014 -->
+  <TABLE border=1>
+  <TR> <TH> country </TH> <TH> continent </TH> <TH> intecept </TH> <TH> slope </TH>  </TR>
+  <TR> <TD> Senegal </TD> <TD> Africa </TD> <TD align="right"> 36.75 </TD> <TD align="right"> 0.50 </TD> </TR>
+  <TR> <TD> Oman </TD> <TD> Asia </TD> <TD align="right"> 37.21 </TD> <TD align="right"> 0.77 </TD> </TR>
+  <TR> <TD> Central African Republic </TD> <TD> Africa </TD> <TD align="right"> 38.81 </TD> <TD align="right"> 0.18 </TD> </TR>
+  <TR> <TD> Comoros </TD> <TD> Africa </TD> <TD align="right"> 40.00 </TD> <TD align="right"> 0.45 </TD> </TR>
+  <TR> <TD> Egypt </TD> <TD> Africa </TD> <TD align="right"> 40.97 </TD> <TD align="right"> 0.56 </TD> </TR>
+  <TR> <TD> Myanmar </TD> <TD> Asia </TD> <TD align="right"> 41.41 </TD> <TD align="right"> 0.43 </TD> </TR>
+  <TR> <TD> West Bank and Gaza </TD> <TD> Asia </TD> <TD align="right"> 43.80 </TD> <TD align="right"> 0.60 </TD> </TR>
+  <TR> <TD> Dominican Republic </TD> <TD> Americas </TD> <TD align="right"> 48.60 </TD> <TD align="right"> 0.47 </TD> </TR>
+  <TR> <TD> Ecuador </TD> <TD> Americas </TD> <TD align="right"> 49.07 </TD> <TD align="right"> 0.50 </TD> </TR>
+  <TR> <TD> Korea, Dem. Rep. </TD> <TD> Asia </TD> <TD align="right"> 54.91 </TD> <TD align="right"> 0.32 </TD> </TR>
+  <TR> <TD> Mauritius </TD> <TD> Africa </TD> <TD align="right"> 55.37 </TD> <TD align="right"> 0.35 </TD> </TR>
+  <TR> <TD> Lebanon </TD> <TD> Asia </TD> <TD align="right"> 58.69 </TD> <TD align="right"> 0.26 </TD> </TR>
+  <TR> <TD> Argentina </TD> <TD> Americas </TD> <TD align="right"> 62.69 </TD> <TD align="right"> 0.23 </TD> </TR>
+  <TR> <TD> Slovak Republic </TD> <TD> Europe </TD> <TD align="right"> 67.01 </TD> <TD align="right"> 0.13 </TD> </TR>
+  <TR> <TD> Germany </TD> <TD> Europe </TD> <TD align="right"> 67.57 </TD> <TD align="right"> 0.21 </TD> </TR>
+  </TABLE>
+  > 
+  > # Q & A
+  > (yearMin <- min(gDat$year))
+[1] 1952
+> jFun <- function(x) {
+  +     estCoefs <-coef(lm(lifeExp ~ I(year - yearMin), x))
+  +     names(estCoefs) <- c("intercept", "slope")
+  +     return(estCoefs)
+  + }
+> jCoefs <- ddply(gDat, ~country, jFun)
+> head(jCoefs)
+country intercept     slope
+1 Afghanistan  29.90729 0.2753287
+2     Albania  59.22913 0.3346832
+3     Algeria  43.37497 0.5692797
+4      Angola  32.12665 0.2093399
+5   Argentina  62.68844 0.2317084
+6   Australia  68.40051 0.2277238
+> 
+  > jFunTwoARgs <- function(x, cvShift = 0) {
+    +     estCoefs <- coef(lm(lifeExp ~ I(year - cvShift), x))
+    +     names(estCoefs) <- c("intercept", "slope")
+    +     return(estCoefs)
+    + }
+> jCoefsSilly <- ddply(gDat, ~country, jFunTwoArgs)
+Error in llply(.data = .data, .fun = .fun, ..., .progress = .progress,  : 
+                 object 'jFunTwoArgs' not found
+               > jCoefsSilly <- ddply(gDat, ~country, jFunTwoARgs)
+               > head(jCoefsSilly)
+               country  intercept     slope
+               1 Afghanistan  -507.5343 0.2753287
+               2     Albania  -594.0725 0.3346832
+               3     Algeria -1067.8590 0.5692797
+               4      Angola  -376.5048 0.2093399
+               5   Argentina  -389.6063 0.2317084
+               6   Australia  -376.1163 0.2277238
+               > jCoefsSane <- ddply(gDat, ~country, jFunTwoARgs, cvShift = 1952)
+               > head(jCoefsSane)
+               country intercept     slope
+               1 Afghanistan  29.90729 0.2753287
+               2     Albania  59.22913 0.3346832
+               3     Algeria  43.37497 0.5692797
+               4      Angola  32.12665 0.2093399
+               5   Argentina  62.68844 0.2317084
+               6   Australia  68.40051 0.2277238
+               > jCoefsBest <- ddply(gDat, ~country, jFunTwoARgs, cvShift = min(gDat$year))
+               > head(jCoefsBest)
+               country intercept     slope
+               1 Afghanistan  29.90729 0.2753287
+               2     Albania  59.22913 0.3346832
+               3     Algeria  43.37497 0.5692797
+               4      Angola  32.12665 0.2093399
+               5   Argentina  62.68844 0.2317084
+               6   Australia  68.40051 0.2277238
---
+++ Problem Set 3/Part III.R
@@ -0,0 +1,206 @@
+> cars <- "http://www.stat.berkeley.edu/classes/s133/data/cars.tab"
+> cars <- read.delim(file = cars)
+> head(cars)
+Country                       Car  MPG Weight Drive_Ratio Horsepower Displacement Cylinders
+1    U.S.        Buick Estate Wagon 16.9  4.360        2.73        155          350         8
+2    U.S. Ford Country Squire Wagon 15.5  4.054        2.26        142          351         8
+3    U.S.        Chevy Malibu Wagon 19.2  3.605        2.56        125          267         8
+4    U.S.    Chrysler LeBaron Wagon 18.5  3.940        2.45        150          360         8
+5    U.S.                  Chevette 30.0  2.155        3.70         68           98         4
+6   Japan             Toyota Corona 27.5  2.560        3.05         95          134         4
+> 
+  > cars.use = cars[, -c(1, 2)]
+> medians = apply(cars.use, 2, median)
+> mads = apply(cars.use, 2, mad)
+> cars.use = scale(cars.use, center = medians, scale = mods)
+Error in scale.default(cars.use, center = medians, scale = mods) : 
+  object 'mods' not found
+> cars.use = scale(cars.use, center = medians, scale = mads)
+> cars.dist = dist(cars.use)
+> cars.hclust = hclust(cars.dist)
+> plot(cars.hclust, labels - cars$Car, main = "Default from hclust")
+Error in if (is.logical(labels) && !labels) character(n + 1L) else as.character(labels) : 
+  missing value where TRUE/FALSE needed
+In addition: Warning message:
+  In Ops.factor(labels, cars$Car) : - not meaningful for factors
+> plot(cars.hclust, labels = cars$Car, main = "Default from hclust")
+> groups.3 = cutree(cars.hclust, 3)
+> table(groups.3)
+groups.3
+1  2  3 
+8 20 10 
+> counts = sapply(2:6, function(ncl) table(cutree(cars.hclust, ncl)))
+> names(counts) = 2:6
+> counts
+$`2`
+
+1  2 
+18 20 
+
+$`3`
+
+1  2  3 
+8 20 10 
+
+$`4`
+
+1  2  3  4 
+8 17  3 10 
+
+$`5`
+
+1  2  3  4  5 
+8 11  6  3 10 
+
+$`6`
+
+1  2  3  4  5  6 
+8 11  6  3  5  5 
+
+> cars$Car[groups.3 ==1]
+[1] Buick Estate Wagon        Ford Country Squire Wagon Chevy Malibu Wagon        Chrysler LeBaron Wagon    Chevy Caprice Classic    
+[6] Ford LTD                  Mercury Grand Marquis     Dodge St Regis           
+38 Levels: AMC Concord D/L AMC Spirit Audi 5000 BMW 320i Buick Century Special Buick Estate Wagon Buick Skylark ... VW Scirocco
+> sapply(unique(groups.3), function(g) cars$Car[groups.3 == g])
+[[1]]
+[1] Buick Estate Wagon        Ford Country Squire Wagon Chevy Malibu Wagon        Chrysler LeBaron Wagon    Chevy Caprice Classic    
+[6] Ford LTD                  Mercury Grand Marquis     Dodge St Regis           
+38 Levels: AMC Concord D/L AMC Spirit Audi 5000 BMW 320i Buick Century Special Buick Estate Wagon Buick Skylark ... VW Scirocco
+
+[[2]]
+[1] Chevette         Toyota Corona    Datsun 510       Dodge Omni       Audi 5000        Saab 99 GLE      Ford Mustang 4   Mazda GLC       
+[9] Dodge Colt       AMC Spirit       VW Scirocco      Honda Accord LX  Buick Skylark    Pontiac Phoenix  Plymouth Horizon Datsun 210      
+[17] Fiat Strada      VW Dasher        BMW 320i         VW Rabbit       
+38 Levels: AMC Concord D/L AMC Spirit Audi 5000 BMW 320i Buick Century Special Buick Estate Wagon Buick Skylark ... VW Scirocco
+
+[[3]]
+[1] Volvo 240 GL          Peugeot 694 SL        Buick Century Special Mercury Zephyr        Dodge Aspen           AMC Concord D/L      
+[7] Ford Mustang Ghia     Chevy Citation        Olds Omega            Datsun 810           
+38 Levels: AMC Concord D/L AMC Spirit Audi 5000 BMW 320i Buick Century Special Buick Estate Wagon Buick Skylark ... VW Scirocco
+
+> groups.4 = cutree(cars.hclust, 4)
+> sapply(unique(groups.4), function(g) cars$Car[groups.4 == g])
+[[1]]
+[1] Buick Estate Wagon        Ford Country Squire Wagon Chevy Malibu Wagon        Chrysler LeBaron Wagon    Chevy Caprice Classic    
+[6] Ford LTD                  Mercury Grand Marquis     Dodge St Regis           
+38 Levels: AMC Concord D/L AMC Spirit Audi 5000 BMW 320i Buick Century Special Buick Estate Wagon Buick Skylark ... VW Scirocco
+
+[[2]]
+[1] Chevette         Toyota Corona    Datsun 510       Dodge Omni       Ford Mustang 4   Mazda GLC        Dodge Colt       AMC Spirit      
+[9] VW Scirocco      Honda Accord LX  Buick Skylark    Pontiac Phoenix  Plymouth Horizon Datsun 210       Fiat Strada      VW Dasher       
+[17] VW Rabbit       
+38 Levels: AMC Concord D/L AMC Spirit Audi 5000 BMW 320i Buick Century Special Buick Estate Wagon Buick Skylark ... VW Scirocco
+
+[[3]]
+[1] Audi 5000   Saab 99 GLE BMW 320i   
+38 Levels: AMC Concord D/L AMC Spirit Audi 5000 BMW 320i Buick Century Special Buick Estate Wagon Buick Skylark ... VW Scirocco
+
+[[4]]
+[1] Volvo 240 GL          Peugeot 694 SL        Buick Century Special Mercury Zephyr        Dodge Aspen           AMC Concord D/L      
+[7] Ford Mustang Ghia     Chevy Citation        Olds Omega            Datsun 810           
+38 Levels: AMC Concord D/L AMC Spirit Audi 5000 BMW 320i Buick Century Special Buick Estate Wagon Buick Skylark ... VW Scirocco
+
+> table(groups.3, cars$Country)
+
+groups.3 France Germany Italy Japan Sweden U.S.
+1      0       0     0     0      0    8
+2      0       5     1     6      1    7
+3      1       0     0     1      1    7
+> aggregate(cars.use, list(groups.3), median)
+Group.1        MPG     Weight Drive_Ratio Horsepower Displacement  Cylinders
+1       1 -0.7945273  1.5051136  -0.9133729  1.0476133    2.4775849  4.7214353
+2       2  0.6859228 -0.5870568   0.5269459 -0.6027364   -0.5809970 -0.6744908
+3       3 -0.4058377  0.5246039  -0.1686227  0.3587717    0.3272282  2.0234723
+> a3 = aggregate(cars[, -c(1, 2)], list(groups.3), median)
+> data.frame(Cluster = a3[, 1], Freq = as.vector(table(groups.3)), a3[, -1])
+Cluster Freq   MPG Weight Drive_Ratio Horsepower Displacement Cylinders
+1       1    8 17.30  3.890       2.430      136.5          334         8
+2       2   20 30.25  2.215       3.455       79.0          105         4
+3       3   10 20.70  3.105       2.960      112.5          173         6
+> a4 = aggregate(cars[, -c(1, 2)], list(groups.4), median)
+> data.frame(Cluster = a4[, 1], Freq = as.vector(table(groups.4)), a4[, -1])
+Cluster Freq  MPG Weight Drive_Ratio Horsepower Displacement Cylinders
+1       1    8 17.3  3.890        2.43      136.5          334         8
+2       2   17 30.9  2.190        3.37       75.0           98         4
+3       3    3 21.5  2.795        3.77      110.0          121         4
+4       4   10 20.7  3.105        2.96      112.5          173         6
+> 
+  > ## K-Means Clustering
+  > library(rattle)
+Rattle: A free graphical interface for data mining with R.
+Version 3.0.2 r169 Copyright (c) 2006-2013 Togaware Pty Ltd.
+Type 'rattle()' to shake, rattle, and roll your data.
+Warning message:
+  package 'rattle' was built under R version 3.0.3 
+> library(NbClust)
+> library(flexclust)
+Loading required package: grid
+Loading required package: lattice
+Loading required package: modeltools
+Loading required package: stats4
+Warning messages:
+  1: package 'flexclust' was built under R version 3.0.3 
+2: package 'lattice' was built under R version 3.0.3 
+> 
+  > wssplot <- function(data, nc = 15, seed = 1234) {
+    +     wss <- (nrow(data) - 1) * sum(apply(data, 2, var))
+    +     for (i in 2:nc) {
+      +         set.seed(seed)
+      +         wss[i] <- sum(kmeans(data, centers = i)$withinss)
+      +     }
+    +     plot(1:nc, wss, type = "b", xlab = "Number of Clusters", ylab = "within groups sum of squares")
+    + }
+> data(wine, package = "rattle")
+> head(wine)
+Type Alcohol Malic  Ash Alcalinity Magnesium Phenols Flavanoids Nonflavanoids Proanthocyanins Color  Hue Dilution Proline
+1    1   14.23  1.71 2.43       15.6       127    2.80       3.06          0.28            2.29  5.64 1.04     3.92    1065
+2    1   13.20  1.78 2.14       11.2       100    2.65       2.76          0.26            1.28  4.38 1.05     3.40    1050
+3    1   13.16  2.36 2.67       18.6       101    2.80       3.24          0.30            2.81  5.68 1.03     3.17    1185
+4    1   14.37  1.95 2.50       16.8       113    3.85       3.49          0.24            2.18  7.80 0.86     3.45    1480
+5    1   13.24  2.59 2.87       21.0       118    2.80       2.69          0.39            1.82  4.32 1.04     2.93     735
+6    1   14.20  1.76 2.45       15.2       112    3.27       3.39          0.34            1.97  6.75 1.05     2.85    1450
+> df <- scale(wine[-1])
+> wssplot(df)
+> set.seed(1234)
+> nc <- NbClust(df, min.nc = 2, max.nc = 15, method = "kmeans")
+[1] "*** : The Hubert index is a graphical method of determining the number of clusters. In the plot of Hubert index, we seek a significant knee that corresponds to a significant increase of the value of the measure i.e the significant peak in Hubert index second differences plot."
+[1] "*** : The D index is a graphical method of determining the number of clusters. In the plot of D index, we seek a significant knee (the significant peak in Dindex second differences plot) that corresponds to a significant increase of the value of the measure."
+[1] "Pseudot2 and Frey indices can be applied only to hierarchical methods "
+[1] "All 178 observations were used."
+> table(nc$Best.n[1, ])
+
+0  2  3  8 13 14 15 
+2  3 14  1  2  1  1 
+> barplot(table(nc$Best.n[1, ]), xlab = "Number of Clusters", ylab = "Number of Criteria", main = "Number of Clusters Chosen by 26 Criteria")
+> set.seed(1234)
+> fit.km <- kmeans(df, 3, nstart = 25)
+> fit.km$size
+[1] 62 65 51
+> fit.km$centers
+Alcohol      Malic        Ash Alcalinity   Magnesium     Phenols  Flavanoids Nonflavanoids Proanthocyanins      Color
+1  0.8328826 -0.3029551  0.3636801 -0.6084749  0.57596208  0.88274724  0.97506900   -0.56050853      0.57865427  0.1705823
+2 -0.9234669 -0.3929331 -0.4931257  0.1701220 -0.49032869 -0.07576891  0.02075402   -0.03343924      0.05810161 -0.8993770
+3  0.1644436  0.8690954  0.1863726  0.5228924 -0.07526047 -0.97657548 -1.21182921    0.72402116     -0.77751312  0.9388902
+Hue   Dilution    Proline
+1  0.4726504  0.7770551  1.1220202
+2  0.4605046  0.2700025 -0.7517257
+3 -1.1615122 -1.2887761 -0.4059428
+> aggregate(wine[-1], by = list(cluster = fit.km$cluster), mean)
+cluster  Alcohol    Malic      Ash Alcalinity Magnesium  Phenols Flavanoids Nonflavanoids Proanthocyanins    Color
+1       1 13.67677 1.997903 2.466290   17.46290 107.96774 2.847581  3.0032258     0.2920968        1.922097 5.453548
+2       2 12.25092 1.897385 2.231231   20.06308  92.73846 2.247692  2.0500000     0.3576923        1.624154 2.973077
+3       3 13.13412 3.307255 2.417647   21.24118  98.66667 1.683922  0.8188235     0.4519608        1.145882 7.234706
+Hue Dilution   Proline
+1 1.0654839 3.163387 1100.2258
+2 1.0627077 2.803385  510.1692
+3 0.6919608 1.696667  619.0588
+> ct.km <- table(wine$Type, fit.km$cluster)
+> ct.km
+
+1  2  3
+1 59  0  0
+2  3 65  3
+3  0  0 48
+> randIndex(ct.km)
+ARI 
+0.897495  
